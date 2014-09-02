613716cc58bfe7a7749e9abeee4eb25a8b2a6620
guff <guff>
2014-09-02 15:02:56 +0800

    add
    add detailViewController

---
+++ TestSwift/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  TestSwift
+//
+//  Created by retygu on 14-9-2.
+//  Copyright (c) 2014年 retygu. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DetailViewController: UIViewController {
+    
+}
